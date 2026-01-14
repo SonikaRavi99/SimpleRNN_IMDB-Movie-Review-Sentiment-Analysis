0affa88c2524cac9fc5c73b5a7d2942c87dedf40
SonikaRavi <136161367+SonikaRavi99@users.noreply.github.com>
2026-01-14 11:10:05 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
